8d280a3a12a1ffb1cc17aaf7db514ef76fbccd08
[Наталья] <kuznetsovanatalex@gmail.com>
2023-12-03 18:40:31 +0500

    Ex7.Сохранение №1

--- lection1_Ex7_magic/Program.cs
+++ lection1_Ex7_magic/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+//Console.SetCursorePosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 30, ya = 1,
+    xb = 1, yb = 30,
+    xc = 60, yc = 30;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+
+int count = 10;
+
+while (count < 10 )
+{
+    int what = new Random().Next(0,3)
+}
